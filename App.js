c745b4bfb79514c6ba05a9134711b8465adfb026
Joao Victor Negreiros <joaovictornsv@gmail.com>
2021-07-05 16:08:07 -0300

    feat: Navigation finalizada

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import 'intl';
-import 'intl/locale-data/jsonp/pt-BR'
-import Carrinho from './src/telas/Carrinho';
+import 'intl/locale-data/jsonp/pt-BR';
+
+import Rotas from './src/Rotas';
+import TelaPadrao from './src/componentes/TelaPadrao';
 
 export default function App() {
-  return <Carrinho />
+  return <TelaPadrao>
+    <Rotas />
+  </TelaPadrao>
 }
--- src/telas/Carrinho/index.js
+++ src/telas/Carrinho/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { FlatList } from 'react-native';
 import Item from './Item';
-import TelaPadrao from '../../componentes/TelaPadrao';
 import StatusCarrinho from '../../componentes/StatusCarrinho';
 import estilosGlobal from '../../estilos';
 
@@ -32,12 +31,12 @@ const servicos = [
 export default function Carrinho() {
   const total = servicos.reduce((soma, { preco, quantidade }) => soma + (preco*quantidade), 0);
 
-  return <TelaPadrao>
+  return <>
     <StatusCarrinho total={total}/>
       <FlatList
         data={servicos}
         renderItem={({ item }) => <Item {...item} />}
         keyExtractor={({ id }) => String(id)}
       />
-  </TelaPadrao>
-} 
+  </>
+}
